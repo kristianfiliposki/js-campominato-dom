3d40ff0f61fa0e18fc2a2360a0a679fd5d337557
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-09 09:50:35 +0100

    problems function game

--- java/main.js
+++ java/main.js
@@ -1,5 +1,8 @@
 let boxes="";
 let PcNs=[];
+const container=document.getElementById("container");
+let box=document.querySelector("#container div")
+
 
 /* scelgo la modalita */
 
@@ -27,7 +30,6 @@ document.getElementById("play").addEventListener("click",function(){
     console.log(boxes)
     /* se facile */
     if(boxes==100){
-        const container=document.getElementById("container");
         let box=document.querySelector("#container div")
         let newDiv = document.createElement("div");
         container.innerHTML="";
@@ -38,6 +40,7 @@ document.getElementById("play").addEventListener("click",function(){
             const numbers=n;
             n++;
             GenBox(n);          
+            Game();
         }
 
         function GenBox(){
@@ -45,14 +48,16 @@ document.getElementById("play").addEventListener("click",function(){
             newDiv.innerHTML+=(n)
             container.append(newDiv);
             newDiv.classList.add("box-facile");
+            
             newDiv.addEventListener("click",function(){
-            newDiv.classList.toggle('highlight');
+                if(PcN==n){console.log("hai perso")}
+                newDiv.classList.toggle('highlight');
             })
-        }    
+        }
+            
     }
     /*se medio  */
     else if(boxes==81){
-        const container=document.getElementById("container");
         let box=document.querySelector("#container div")
         let newDiv = document.createElement("div");
         container.innerHTML="";
@@ -71,15 +76,20 @@ document.getElementById("play").addEventListener("click",function(){
             newDiv.innerHTML+=(n)
             container.append(newDiv);
             newDiv.classList.add("box-medio");
+            for (i = 0;i< 16;i++ ) {
+                PcN=`${Math.floor(Math.random() * 81)+1}`;
+                PcNs.push(PcN);
+                console.log(PcNs)
+            }
             newDiv.addEventListener("click",function(){
-            newDiv.classList.toggle('highlight');
+                if(PcN==n){console.log("hai perso")}
+                newDiv.classList.toggle('highlight');
             })
         }   
 
     }
     /* se difficile */
     else if(boxes==49){
-        const container=document.getElementById("container");
         let box=document.querySelector("#container div")
         let newDiv = document.createElement("div");
         container.innerHTML="";
@@ -99,27 +109,54 @@ document.getElementById("play").addEventListener("click",function(){
             newDiv.innerHTML+=(n)
             container.append(newDiv);
             newDiv.classList.add("box-difficile");
+            
+            PcN=`${Math.floor(Math.random() * 49)}`;
+            PcNs.push(PcN);
+            console.log(PcNs)
             newDiv.addEventListener("click",function(){
+                
+            if(PcN==n){console.log("hai perso")}
             newDiv.classList.toggle('highlight');
             })
+
         }   
 
     }
     
 })
 
-/* function GenPc(){
-} */
+function Game(){
+    PcN=`${Math.floor(Math.random() * 49)}`;
+            PcNs.push(PcN);
+            console.log(PcNs)
+            newDiv.addEventListener("click",function(){
 
-let PcN=("");
-let Pcn = 0;
-while ( PcN < 16 ) {
-    PcN=`${Math.floor(Math.random() * 16)+1}`;
-    
-    if((PcNs[PcN])==false){
-        PcNs.push(PcN);
-    }   
+            })
 
+            return PcNs
+
+} 
+
+
+
+/* let i = 0;
+while (i< 16 ) {
+    PcN=`${Math.floor(Math.random() * 100)+1}`;
+    i++
+    PcNs.push(PcN);
 }
-console.log(PcNs)
+
+
+
+console.log(PcNs) */
+/* box.addEventListener("click",function(){
+   if(box.innerHTML==PcNs[0]||box.innerHTML==PcNs[1]||box.innerHTML==PcNs[2]||box.innerHTML==PcNs[3]||box.innerHTML==PcNs[4]||box.innerHTML==PcNs[5]||box.innerHTML==PcNs[6]||box.innerHTML==PcNs[7]||box.innerHTML==PcNs[8]||box.innerHTML==PcNs[9]||box.innerHTML==PcNs[10]||box.innerHTML==PcNs[11]||box.innerHTML==PcNs[12]||box.innerHTML==PcNs[13]||box.innerHTML==PcNs[14]||box.innerHTML==PcNs[15]){
+    
+    if(box.innerHTML==PcNs[0]){
+        console.log("hai perso");
+    }
+})
+ */
+
+
 
