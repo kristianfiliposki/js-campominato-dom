13420af5cead1e2f1ebe48a796108f3baf2925ff
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-11-13 01:52:23 +0100

    finito!

--- java/main.js
+++ java/main.js
@@ -1,142 +1,150 @@
+/* variabili */
 let boxes="";
+let numeriGenerati=[]
 const container=document.getElementById("container");
 let box=document.querySelector("#container div")
-PcNumb1=console.log(Math.floor(Math.random() * 49)+1);
-PcNumb2=console.log(Math.floor(Math.random() * 49)+1);
-PcNumb3=console.log(Math.floor(Math.random() * 49)+1);
-PcNumb4=console.log(Math.floor(Math.random() * 49)+1);
-PcNumb5=console.log(Math.floor(Math.random() * 49)+1);
-PcNumb6=console.log(Math.floor(Math.random() * 49)+1);
+let newDivs=[]
 
 
-/* scelgo la modalita */
+
+
+
+/* scelgo la modalita con cui giocare*/
 
 /* facile */
 document.getElementById("facile").addEventListener("click",function(){
-    boxes=parseInt(100);
+    boxes=100;
+    console.log(`ciao le caselle sono ${boxes}`)
+
 })
 
 /* medio */
 document.getElementById("medio").addEventListener("click",function(){
-    boxes=parseInt(81);
+    boxes=81;
     console.log(`ciao le caselle sono ${boxes}`)
+    
+    
 })
 
 /* difficile */
 document.getElementById("difficile").addEventListener("click",function(){
-    boxes=parseInt(49);
-    console.log(boxes) 
+    boxes=49;
+    console.log(`ciao le caselle sono ${boxes}`)
+    
 })
 
 
 /* play */
-document.getElementById("play").addEventListener("click",function(){
-    
-    console.log(boxes)
+document.getElementById("play").addEventListener("click",function(){    
     /* se facile */
     if(boxes==100){
+        
+
+        for (let i = 0; i < 6; i++) {
+            PcNumb=Math.floor(Math.random() * 100)+1;
+            numeriGenerati.push(PcNumb);
+            console.log(numeriGenerati)
+        }
+        
+        
         let box=document.querySelector("#container div")
-        let newDiv = document.createElement("div");
         container.innerHTML="";
+
     
-    
-        let n = 0    
-        while(  n < boxes) {
-            const numbers=n;
-            n++;
-            GenBox(n);          
-        }
-
-        function GenBox(){
-            var newDiv = document.createElement("div");
-            newDiv.innerHTML+=(n)
-            container.append(newDiv);
-            newDiv.classList.add("box-facile");
-            clicked(newDiv)
-        }   
-            
-    }
-    /*se medio  */
-    else if(boxes==81){
-        let box=document.querySelector("#container div")
-        let newDiv = document.createElement("div");
-        container.innerHTML="";
-
-        
-        boxes=81;
-        let n = 0    
-        while(  n < boxes) {
-            const numbers=n;
-            n++;
-            GenBox(n);
-        }
-
-        function GenBox(){
-            var newDiv = document.createElement("div");
-            newDiv.innerHTML+=(n)
-            container.append(newDiv);
-            newDiv.classList.add("box-medio");
-            clicked(newDiv)
-        }    
-
-    }
-    /* se difficile */
-    else if(boxes==49){
-        let box=document.querySelector("#container div")
-        let newDiv = document.createElement("div");
-        container.innerHTML="";
-        
-        
-        
-        boxes=49;
-        let n = 0    
-        while(  n < boxes) {
-            const numbers=n;
-            n++;
-            GenBox(n)
-        }
-
-        function GenBox(){
+        for(let n = 1 ;n <= boxes;n++) {
             newDiv = document.createElement("div");
-            newDiv.innerHTML+=(n)
+            newDiv.classList.add("box-facile");
+            newDiv.innerHTML=(n)
+            newDivs.push(n)
             container.append(newDiv);
-            newDiv.classList.add("box-difficile");
-            clicked(newDiv)            
-        }   
+            clicked(newDiv,n)  
+        }
         
+
+        function clicked(div,number){
+            div.addEventListener("click",function(){
+            div.classList.add('highlight');
+        if(numeriGenerati.includes(number)){
+            div.classList.remove('highlight');
+            div.classList.add('errorbox');}
+        })
     }
-    
-})
-
-function clicked(newDiv){
-        newDiv.addEventListener("click",function(){
-        newDiv.classList.add('highlight');
-    if(newDiv.innerHTML==(PcNumb1)||(PcNumb2)||(PcNumb3)||(PcNumb4)||(PcNumb5)||(PcNumb6)){
-        newDiv.classList.remove('highlight');
-        newDiv.classList.add('errorbox');}
-    })
-}
- 
-
-
-/* function GenNum() {
-    let PcNumbers=[];
-    for (let i = 0; i < 16; i++) {
-        PcNumb=console.log(Math.floor(Math.random(i) * 49)+1);
-        PcNumbers.push(PcNumb)
-    }
-    return(PcNumbers)
-}  */
-
-
-/* box.addEventListener("click",function(){
-   if(box.innerHTML==PcNs[0]||box.innerHTML==PcNs[1]||box.innerHTML==PcNs[2]||box.innerHTML==PcNs[3]||box.innerHTML==PcNs[4]||box.innerHTML==PcNs[5]||box.innerHTML==PcNs[6]||box.innerHTML==PcNs[7]||box.innerHTML==PcNs[8]||box.innerHTML==PcNs[9]||box.innerHTML==PcNs[10]||box.innerHTML==PcNs[11]||box.innerHTML==PcNs[12]||box.innerHTML==PcNs[13]||box.innerHTML==PcNs[14]||box.innerHTML==PcNs[15]){
-    
-    if(box.innerHTML==PcNs[0]){
-        console.log("hai perso");
+  
     }
 })
- */
-
-
-
+    /*se medio  */
+    document.getElementById("play").addEventListener("click",function(){    
+        if(boxes==81){
+            
+    
+            for (let i = 0; i < 6; i++) {
+                PcNumb=Math.floor(Math.random() * 81)+1;
+                numeriGenerati.push(PcNumb);
+                console.log(numeriGenerati)
+            }
+            
+            
+            let box=document.querySelector("#container div")
+            container.innerHTML="";
+    
+        
+            for(let n = 1 ;n <= boxes;n++) {
+                newDiv = document.createElement("div");
+                newDiv.classList.add("box-medio");
+                newDiv.innerHTML=(n)
+                newDivs.push(n)
+                container.append(newDiv);
+                clicked(newDiv,n)  
+            }
+            
+    
+            function clicked(div,number){
+                div.addEventListener("click",function(){
+                div.classList.add('highlight');
+            if(numeriGenerati.includes(number)){
+                div.classList.remove('highlight');
+                div.classList.add('errorbox');}
+            })
+        }
+      
+        }
+})
+    
+    /* se difficile */
+    document.getElementById("play").addEventListener("click",function(){    
+        if(boxes==49){
+            
+    
+            for (let i = 0; i < 6; i++) {
+                PcNumb=Math.floor(Math.random() * 49)+1;
+                numeriGenerati.push(PcNumb);
+                console.log(numeriGenerati)
+            }
+            
+            
+            let box=document.querySelector("#container div")
+            container.innerHTML="";
+    
+        
+            for(let n = 1 ;n <= boxes;n++) {
+                newDiv = document.createElement("div");
+                newDiv.classList.add("box-difficile");
+                newDiv.innerHTML=(n)
+                newDivs.push(n)
+                container.append(newDiv);
+                clicked(newDiv,n)  
+            }
+            
+    
+            function clicked(div,number){
+                div.addEventListener("click",function(){
+                div.classList.add('highlight');
+            if(numeriGenerati.includes(number)){
+                div.classList.remove('highlight');
+                div.classList.add('errorbox');}
+            })
+        }
+      
+        }
+})
+/* ti ho odiato!!! */
